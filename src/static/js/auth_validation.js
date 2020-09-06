4daad1129d8fa64957316e15fc8dc2c0ebbec4b7
Elad Bergrin <EladB1@users.noreply.github.com>
2020-09-06 16:27:14 -0400

    Fine tuned the validations on the sign up page.

---
+++ src/static/js/auth_validation.js
@@ -0,0 +1,13 @@
+function passwords_match() {
+	let psswd = document.getElementById('password_input');
+  let confirm = document.getElementById('confirm_password_input');
+  return confirm.value === psswd.value;
+}
+
+function handle_registration_submit() {
+	let err = document.getElementById('errorMessages');
+  err.innerHTML = '';
+  check = passwords_match();
+  if (!check)
+  	err.innerHTML += 'Passwords do not match.<br>';
+}
